2c53c20e76bfd9e9738c1206fa7d54d91860ad36
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2019-01-18 18:32:29 +0200

    started implementation of the E-step

--- Diploma.Algorithms/EM/EMAlgorithm.cs
+++ Diploma.Algorithms/EM/EMAlgorithm.cs
@@ -16,13 +16,11 @@ namespace Diploma.Algorithms.EM
         {
             public double MStruct;
             public double GStruct;
-            //public double Probability;
 
-            public Parameters(double m, double g)//, double probability)
+            public Parameters(double m, double g)
             {
                 MStruct = m;
                 GStruct = g;
-                //Probability = probability;
             }
         }
         protected List<Parameters> HiddenVector { get; set; }
@@ -43,9 +41,48 @@ namespace Diploma.Algorithms.EM
                 //TODO what interval should be?
                 var m = random.Next(5) + 0.1 * random.Next(10);
                 var g = random.Next(5) + 0.1 * random.Next(10);
-                //var probability = Distribution.CountProbabilityFunctionResult(m, g)
                 HiddenVector.Add(new Parameters(m, g));
             }
+        }
+
+        private void EStep()
+        {
+            var probabilities = CountProbabilitiesForEachPoint();
+
+        }
+
+        private double[,] CountProbabilitiesForEachPoint()
+        {
+            var probabilities = new double[DataSetValues.Count, AmountOfClusters];
+            for (int i = 0; i < DataSetValues.Count; i++)
+            {
+                for (int j = 0; j < AmountOfClusters; j++)
+                {
+                    probabilities[i, j] = Distribution.CountProbabilityFunctionResult(HiddenVector[j].MStruct,
+                        HiddenVector[j].GStruct, DataSetValues[i]);
+                }
+            }
+
+            return probabilities;
+        }
+
+        private double[] CountAverage(double[,] probabilities)
+        {
+            var averages = new double[AmountOfClusters];
+            for (int i = 0; i < AmountOfClusters; i++)
+            {
+                double sumUp = 0;
+                double sumDown = 0;
+                for (int j = 0; j < DataSetValues.Count; j++)
+                {
+                    sumUp += probabilities[j, i] * DataSetValues[j];
+                    sumDown += probabilities[j, i];
+                }
+                //TODO have to finished a method
+                //averages[i] = 
+            }
+
+            return averages;
         }
     }
 }
