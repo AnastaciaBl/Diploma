fa3ce057ace9fe5fc4095ccaf4405b5609b728ab
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2019-02-22 19:17:22 +0200

    Added test for EM algorithm

--- Diploma.Algorithms.Tests/EMAlgorithmTest.cs
+++ Diploma.Algorithms.Tests/EMAlgorithmTest.cs
@@ -1,14 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Diploma.Algorithms.Distribution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathNet.Numerics;
+using Diploma.Algorithms.EM;
+using MathNet.Numerics.Distributions;
 
 namespace Diploma.Algorithms.Tests
 {
     [TestClass]
     public class EMAlgorithmTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private List<Parameters> EtalonHiddenVector { get; set; }
+        private int AmountOfElements { get; set; }
+        private int AmountOfClusters { get; set; }
+        private double Eps { get; set; }
+
+        [TestInitialize]
+        public void Setup()
         {
+            EtalonHiddenVector = new List<Parameters>();
+            Eps = 0.0001;
+        }
+
+        [TestMethod]
+        public void TestEMAlgorithm()
+        {
+            //Arrange
+            AmountOfElements = 1000;
+            AmountOfClusters = 3;
+            EtalonHiddenVector.Add(new Parameters(-5, 0.2, 0.1));
+            EtalonHiddenVector.Add(new Parameters(5, 0.2, 0.1));
+            EtalonHiddenVector.Add(new Parameters(0, 0.1, 0.8));
+            var sample = GenerateSample();
+            
+            //Act
+            var emAlgorithm = new EMAlgorithm(AmountOfClusters, new NormalDistribution(), sample, Eps);
+            emAlgorithm.SplitOnClusters();
+
+            //Assert
             Assert.IsTrue(false);
+        }
+
+        private List<double> GenerateSample()
+        {
+            var sample = new List<double>();
+            for (int i = 0; i < AmountOfClusters; i++)
+            {
+                int amountOfElementsInCluster = (int) (EtalonHiddenVector[i].СStruct * AmountOfElements);
+                Normal random = new Normal(EtalonHiddenVector[i].MStruct, Math.Sqrt(EtalonHiddenVector[i].GStruct));
+                for (int j = 0; j < amountOfElementsInCluster; j++)
+                {
+                    sample.Add(random.Sample());
+                }
+            }
+            return sample;
         }
     }
 }
