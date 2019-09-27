20f7f1f4ce5b6aaa3e19cf26633a4b27d165e291
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2019-09-27 17:32:22 +0300

    Add realisation for k-means algorithm

---
+++ Diploma.Algorithms/KMeans/KMeansAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using Accord.MachineLearning;
+
+namespace Diploma.Algorithms.Kmeans
+{
+    public class KMeansAlgorithm
+    {
+        public int AmountOfClusters { get; set; }
+        public double[][] DataSet { get; set; }
+        public int[] Labels { get; set; }
+
+        public KMeansAlgorithm(int amountOfClusters, double[] data)
+        {
+            AmountOfClusters = amountOfClusters;
+            DataSet = new[]
+            {
+                data
+            };
+        }
+
+        public void SplitOnClusters()
+        {
+            var kMeans = new KMeans(AmountOfClusters);
+            var clusters = kMeans.Learn(DataSet);
+            Labels = clusters.Decide(DataSet);
+        }
+    }
+}
