9f4b5b3197c15d403a2d876df40ec22ce4fa529d
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2019-11-29 17:07:28 +0200

    Count indexes for hierarchic algorithm

--- Diploma.Algorithms/AgglomerativeHierarchic/AgglomerativeHierarchicAglomera.cs
+++ Diploma.Algorithms/AgglomerativeHierarchic/AgglomerativeHierarchicAglomera.cs
@@ -1,5 +1,6 @@
 ﻿using Aglomera;
 using Aglomera.D3;
+using Aglomera.Evaluation.Internal;
 using Aglomera.Linkage;
 using Diploma.Model;
 using Newtonsoft.Json;
@@ -11,10 +12,16 @@ namespace Diploma.Algorithms.AgglomerativeHierarchic
     {
         private HashSet<DataPoint> DataSet { get; set; }
         private ClusteringResult<DataPoint> Result { get; set; }
+        public List<double> SilhouetteCoefs { get; set; }
+        public List<double> CalinskiHarabaszCoefs { get; set; }
+        public List<double> DaviesBouldinCoefs { get; set; }
 
         public AgglomerativeHierarchicAglomera(List<Patient> patients, double[][] data, int amountOfElements)
         {
             DataSet = CreateDataSet(patients, data, amountOfElements);
+            SilhouetteCoefs = new List<double>();
+            CalinskiHarabaszCoefs = new List<double>();
+            DaviesBouldinCoefs = new List<double>();
         }
 
         private HashSet<DataPoint> CreateDataSet(List<Patient> patients, double[][] data, int amountOfElements)
@@ -39,6 +46,43 @@ namespace Diploma.Algorithms.AgglomerativeHierarchic
             var algorithm = new AgglomerativeClusteringAlgorithm<DataPoint>(linkage);
             Result = algorithm.GetClustering(DataSet);
             CreateJson();
+
+            SilhouetteCoefs = GetSilhouetteCoefficients();
+            CalinskiHarabaszCoefs = GetCalinskiHarabaszCoefficients();
+            DaviesBouldinCoefs = GetDaviesBouldinCoefficients();
+        }
+
+        public List<double> GetSilhouetteCoefficients()
+        {
+            var coefs = new List<double>();
+            var criterion = new SilhouetteCoefficient<DataPoint>(new DataPoint());
+            for (var i = Result.Count - 1; i >= 0; i--)
+            {
+                coefs.Add(criterion.Evaluate(Result[i]));
+            }
+            return coefs;
+        }
+
+        public List<double> GetCalinskiHarabaszCoefficients()
+        {
+            var coefs = new List<double>();
+            var criterion = new SilhouetteCoefficient<DataPoint>(new DataPoint());
+            for (var i = Result.Count - 1; i >= 0; i--)
+            {
+                coefs.Add(criterion.Evaluate(Result[i]));
+            }
+            return coefs;
+        }
+
+        public List<double> GetDaviesBouldinCoefficients()
+        {
+            var coefs = new List<double>();
+            var criterion = new SilhouetteCoefficient<DataPoint>(new DataPoint());
+            for (var i = Result.Count - 1; i >= 0; i--)
+            {
+                coefs.Add(criterion.Evaluate(Result[i]));
+            }
+            return coefs;
         }
     }
 }
